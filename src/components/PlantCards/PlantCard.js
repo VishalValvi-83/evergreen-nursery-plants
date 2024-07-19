5566820677d5214de151ff7c88ae18b0eb4a993c
VishalValvi-83 <vishalvalvi2529@gmail.com>
2024-07-19 19:50:20 +0530

    delete function added

--- src/components/PlantCards/PlantCard.js
+++ src/components/PlantCards/PlantCard.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import './PlantCard.css'
+import Edit from './edit.png'
+import Delete from './delete.png'
+import axios from 'axios'
+import toast from 'react-hot-toast'
 
 function PlantCard({ _id, name, image, description, price }) {
+    const deletePlant = async (id) => {
+        const response = await axios.delete(`${process.env.REACT_APP_API_URL}/plant/${id}`)
+        toast.success(response.data.message)
+        window.location.reload() 
+    }
     return (
         <div className='col-xl-3 col-lg-3 col-md-6 col-sm-12'>
             <div className="card">
@@ -15,6 +24,16 @@ function PlantCard({ _id, name, image, description, price }) {
                     <button href="/" className="btn">
                         Add to Cart
                     </button>
+                </div>
+                <div className='config-bnt'>
+                    <button className='edit-btn ' > <img className='' src={Edit} alt='' /></button>
+
+                    <button className='delete-btn ms-2' onClick={()=>{
+                        deletePlant(_id)
+                    }} >
+                        <img className='' src={Delete} alt='' />
+                    </button>
+
                 </div>
             </div>
         </div>
