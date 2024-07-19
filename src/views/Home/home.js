d86b6a1bc718574719e5743e20dc46b9748b242c
VishalValvi-83 <vishalvalvi2529@gmail.com>
2024-07-19 12:08:28 +0530

    initial commit

---
+++ src/views/Home/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import './home.css'
+
+function home() {
+
+  return (
+    <div>home</div>
+  )
+}
+
+export default home
---
+++ src/views/Plantview/plantview.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './plantview.css'
+function plantview() {
+  return (
+    <div>plantview</div>
+  )
+}
+
+export default plantview
